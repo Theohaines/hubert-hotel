a377f42458495f2d560054cbf2dbd43de956cd37
WintrCat <wintrcat.wilson@gmail.com>
2024-01-31 19:39:03 +0000

    Set up webserver

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use("/static", express.static("src/public"));
+
+app.get("/", async (req, res) => {
+    res.sendFile(path.resolve("src/public/pages/game/index.html"));
+});
+
+app.listen(process.env.HTTP_PORT, () => {
+    console.log(`Server running on port ${process.env.HTTP_PORT}`);
+});
